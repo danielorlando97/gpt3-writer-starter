061c0364f03a47f416d31518b2bfa55b2c40a579
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2022-12-01 09:30:01 -0500

    update tutorial

---
+++ pages/api/index.js
@@ -0,0 +1,5 @@
+const generateAction = async (req, res) => {
+  res.status(200).json({ output: "Hello" });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generator", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+    console.log(response);
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -17,7 +48,39 @@ const Home = () => {
             <h2>insert your subtitle here</h2>
           </div>
         </div>
+
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="start typing here"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a
+            className={
+              isGenerating ? "generate-button loading" : "generate-button"
+            }
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+              {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
       </div>
+      <div className="output">
+        <div className="output-header-container">
+          <div className="output-header">
+            <h3>Output</h3>
+          </div>
+        </div>
+        <div className="output-content">
+          <p>{apiOutput}</p>
+        </div>
+      </div>
+
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
